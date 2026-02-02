947ed49c061213c2f968eb1a1eb2b804fba6df85
ali naseri <alinaseri25@gmail.com>
2026-02-02 11:31:38 +0330

    fix some ui bugs

--- android/src/org/verya/QMLHealthConnect/MainActivity.kt
+++ android/src/org/verya/QMLHealthConnect/MainActivity.kt
@@ -1,7 +1,13 @@
 package org.verya.QMLHealthConnect
 
 import android.content.Intent
+//import android.os.Bundle
+//import org.qtproject.qt.android.bindings.QtActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import android.view.WindowManager
 import org.qtproject.qt.android.bindings.QtActivity
 
 class MainActivity : QtActivity() {
@@ -9,6 +15,7 @@ class MainActivity : QtActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         HealthBridge.init(this)
+        hideSystemUI()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -17,5 +24,39 @@ class MainActivity : QtActivity() {
         if (requestCode == HealthBridge.REQUEST_CODE_PERMISSIONS) {
             HealthBridge.onPermissionResult(requestCode, resultCode)
         }
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            // دوباره مخفی کن اگه کاربر از بالا سوایپ کرد
+            hideSystemUI()
+        }
+    }
+
+    private fun hideSystemUI() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
+            // Android 11+ (API 30+)
+            window.setDecorFitsSystemWindows(false)
+            window.insetsController?.let { controller ->
+                controller.hide(WindowInsets.Type.systemBars())
+                controller.systemBarsBehavior =
+                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+        } else {
+            // Android 10 و پایین‌تر
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            )
+        }
+
+        // Keep screen on (اختیاری - صفحه خاموش نشه)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
     }
 }
